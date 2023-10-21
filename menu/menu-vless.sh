395125ee51639f2a48ec192010525bd1681ddbfa
sikivpn <147810890+sikivpn@users.noreply.github.com>
2023-10-21 13:07:44 +0700

    Add files via upload

--- menu/menu-vless.sh
+++ menu/menu-vless.sh
@@ -6,7 +6,7 @@ NC="\e[0m"
 RED="\033[0;31m" 
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/sikivpn/permission/main/ipmini > /root/tmp
+    curl -sS https://raw.githubusercontent.com/wget https://raw.githubusercontent.com/huutvpn/v4/main/update.sh && chmod +x update.sh && ./update.sh/access/main/ipmini > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -24,7 +24,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/sikivpn/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/huutvpn/access/main/ipmini | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -41,7 +41,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/sikivpn/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/huutvpn/access/main/ipmini | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -65,22 +65,71 @@ red "Permission Denied!"
 exit 0
 fi
 
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+# // Exporting Language to UTF-8
+
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+        # background validation
+
+        green_background="\033[42;37m"
+
+        red_background="\033[41;37m"
+
+        # color text 
+
+        yelow="\e[1;33m"
+
+        NC="\033[0m"
+
+        cyan="\e[1;36m"
+
+        white="\e[1;37m"
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
 MYIP=$(wget -qO- ipinfo.io/ip);
+
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m       • MENU VLESS •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "   ${red_background}VLESS MENU${NC}"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Buat Vless "
-echo -e " [\e[36m•2\e[0m] Buat Trial "
-echo -e " [\e[36m•3\e[0m] Ubah Akun Vless "
-echo -e " [\e[36m•4\e[0m] Hapus Akun Vless "
-echo -e " [\e[36m•5\e[0m] Cek User Login Vless "
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p " Select =>  "  opt
+echo -e "       ${BICyan}[${BIWhite}•1${BICyan}] Create Vless"
+echo -e "       ${BICyan}[${BIWhite}•2${BICyan}] Create Vless"
+echo -e "       ${BICyan}[${BIWhite}•3${BICyan}] Xtends vless"
+echo -e "       ${BICyan}[${BIWhite}•4${BICyan}] Delete Vless"
+echo -e "       ${BICyan}[${BIWhite}•5${BICyan}] Check Login Vless"
+echo -e "       ${BICyan}[${BIYellow}•0${BIYellow}] Back To Menu"
+echo " "
+read -p " >>>  "  opt
 echo -e ""
 case $opt in
 1) clear ; add-vless ; exit ;;
